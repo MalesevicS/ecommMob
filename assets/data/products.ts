30719ec9d2cf74d6c703d0bc2b5cc185da5e85e9
sergej_99 <sergejmalesevic8@gmail.com>
2024-04-04 18:05:49 +0200

    init expo project

---
+++ assets/data/products.ts
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id:1,
+        name: "Curry 2",
+        price: 125,
+        image:"../images/Curry2.jpg"
+    },
+    {
+        id:2,
+        name: "Kobe 6",
+        price: 145,
+        image:"../images/Kobe6.jpg"
+    },
+    {
+        id:3,
+        name: "Luka 11",
+        price: 110,
+        image:"../images/Luka1.jpg"
+    },
+    {
+        id:4,
+        name: "Lebron XX",
+        price: 175,
+        image:"../images/LebronXX.jpg"
+    },
+    {
+        id:5,
+        name: "KD 9",
+        price: 190,
+        image:"../images/KD9.jpg"
+    },
+    {
+        id:6,
+        name: "Jordan 4",
+        price: 325,
+        image:"../images/Jordan4.jpg"
+    },
+]
