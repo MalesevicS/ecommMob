07f36acdd3704e6cce0b31ab008b19d9227ead23
sergej_99 <sergejmalesevic8@gmail.com>
2024-04-12 19:33:37 +0200

    building One

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -3,36 +3,38 @@ const products = [
         id:1,
         name: "Curry 2",
         price: 125,
-        image:"../images/Curry2.jpg"
+        image:"../../../assets/images/Curry2.jpg"
     },
     {
         id:2,
         name: "Kobe 6",
         price: 145,
-        image:"../images/Kobe6.jpg"
+        image:"../../../assets/images/Kobe6.jpg"
     },
     {
         id:3,
         name: "Luka 11",
         price: 110,
-        image:"../images/Luka1.jpg"
+        image:"../../../assets/images/Luka1.jpg"
     },
     {
         id:4,
         name: "Lebron XX",
         price: 175,
-        image:"../images/LebronXX.jpg"
+        image:"../../../assets/images/LebronXX.jpg"
     },
     {
         id:5,
         name: "KD 9",
         price: 190,
-        image:"../images/KD9.jpg"
+        image:"../../../assets/images/KD9.jpg"
     },
     {
         id:6,
         name: "Jordan 4",
         price: 325,
-        image:"../images/Jordan4.jpg"
+        image:"../../../assets/images/Jordan4.jpg"
     },
 ]
+
+export default products;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,46 @@
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { Text, View, } from '@/src/components/Themed';
+import { Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import products from '../../../assets/data/products'; 
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      
+        <View>
+          <Image style={styles?.image} source={{uri: products[0].image}} />
+          <Text style={styles.title}>{products[0].name}</Text>
+          <Text style={styles.price}>${products[0].price}</Text>
+        </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"white",
+    padding:10,
+    borderRadius:15
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  price: {
+    fontSize: 12,
+    color: Colors.light.tint,
+  },
+  image : {
+    width: `100%`,
+    aspectRatio:1
+  }
 });
