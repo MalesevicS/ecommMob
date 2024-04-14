07dd430485381309a17bb4a20832312942c95083
sergej_99 <sergejmalesevic8@gmail.com>
2024-04-14 23:15:23 +0200

    cart_Implement

---
+++ src/app/cart.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StatusBar, Platform, StyleSheet } from 'react-native'
+import React from 'react'
+
+const cart = () => {
+  return (
+    <View>
+      <Text>cart</Text>
+
+      <StatusBar />
+    </View>
+  )
+}
+
+export default cart
+
+const styles = StyleSheet.create({
+    
+})
